a182e2a81fb45011a39ef6d4220929161518bf4c
michael.morristhomas <michael.morristhomas@me.com>
2024-02-11 18:45:46 +0800

    Added company mode

--- init.el
+++ init.el
@@ -6,11 +6,11 @@
 
 (setq backup-inhibited t)
 (setq ring-bell-function 'ignore)
+(setq-default indent-tabs-mode nil)
 
 (column-number-mode 1)
 (global-display-line-numbers-mode t)
-
-(setq-default indent-tabs-mode nil)
+(setq-default display-line-numbers-width 3)
 
 (use-package evil
   :ensure t
@@ -25,25 +25,25 @@
   (setq doom-modeline-icon t)
   (setq doom-modeline-major-mode-color-icon nil))
 
-(use-package format-all
-  :ensure t
-  :hook (before-save . format-all-buffer))
-
-(defun format-elisp-on-save ()
-  (when (eq major-mode 'emacs-lisp-mode)
-    (indent-region (point-min) (point-max))))
-(add-hook 'before-save-hook 'format-elisp-on-save)
-
 (use-package doom-themes
   :ensure t
   :config
   (load-theme 'doom-gruvbox t)
   (doom-themes-org-config))
 
+(use-package format-all
+  :ensure t
+  :hook (before-save . format-all-buffer))
+
+(use-package company
+  :ensure t
+  :bind ("C-." . company-complete)
+  :config
+  (global-company-mode 1))
+
 (use-package rust-mode
   :ensure t
   :config
-  (rust-mode 1)
   (setq rust-format-on-save t))
 
 (use-package swiper
@@ -53,6 +53,11 @@
   :config
   (setq swiper-action-recenter t)
   (setq swiper-include-line-number-in-search t))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
 
 (defun initialize-frame (frame)
   (if (display-graphic-p frame)
@@ -64,12 +69,16 @@
         (set-frame-width frame 138)
         (set-frame-height frame 75)
         (set-frame-position frame 1 5)
-        (setq display-line-numbers-width 3)
         (set-frame-parameter frame 'internal-border-width 2))))
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions #'initialize-frame)
   (initialize-frame (selected-frame)))
+
+(defun format-elisp-on-save ()
+  (when (eq major-mode 'emacs-lisp-mode)
+    (indent-region (point-min) (point-max))))
+(add-hook 'before-save-hook 'format-elisp-on-save)
 
 (defun move-line-up ()
   "Move up the current line."
