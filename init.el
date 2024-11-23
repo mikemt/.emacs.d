a32db3fbb5740d4bf7d8a180c7f2dbe91cb67085
michael.morristhomas <michael.morristhomas@me.com>
2024-11-23 14:31:03 +0800

    fix: general cleanup

--- init.el
+++ init.el
@@ -4,9 +4,7 @@
 ;;;
 ;;; Code:
 
-
-(eval-when-compile
-  (require 'use-package))
+(setenv "LANG" "en_US.UTF-8")
 
 (require 'package)
 (add-to-list 'package-archives
@@ -15,18 +13,21 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
+(when (eq system-type 'windows-nt)
+  (add-to-list 'exec-path (concat (getenv "USERPROFILE") "/scoop/shims")))
+
 (defun format-elisp-on-save ()
   (when (eq major-mode 'emacs-lisp-mode)
     (indent-region (point-min) (point-max))))
 
-(defun move-line-up ()
+(defun _move-line-up ()
   "Move up the current line."
   (interactive)
   (transpose-lines 1)
   (forward-line -2)
   (indent-according-to-mode))
 
-(defun move-line-down ()
+(defun _move-line-down ()
   "Move down the current line."
   (interactive)
   (forward-line 1)
@@ -34,7 +35,7 @@
   (forward-line -1)
   (indent-according-to-mode))
 
-(defun indent-to-next-tab-stop ()
+(defun _indent-to-next-tab-stop ()
   "Indent the current line or region to the next tab stop."
   (interactive)
   (if (use-region-p)
@@ -47,7 +48,7 @@
             (forward-line 1))))
     (indent-to-next-tab-stop-on-line)))
 
-(defun indent-to-next-tab-stop-on-line ()
+(defun _indent-to-next-tab-stop-on-line ()
   "Indent the current line to the next tab stop."
   (let ((next-tab-stop (+ (current-indentation) (- tab-width (mod (current-indentation) tab-width)))))
     (save-excursion
@@ -55,29 +56,29 @@
       (delete-horizontal-space)
       (insert (make-string next-tab-stop ?\s)))))
 
-(defun current-indentation ()
+(defun _current-indentation ()
   "Return the current line's indentation."
   (save-excursion
     (move-beginning-of-line nil)
     (skip-chars-forward " \t")
     (current-column)))
 
-(defun find-file-emacs-init ()
+(defun _find-file-emacs-init ()
   "Open the init.el file."
   (interactive)
   (find-file (expand-file-name "~/.emacs.d/init.el")))
 
-(defun find-file-vscode-settings ()
+(defun _find-file-vscode-settings ()
   "Opens vscode settings.json"
   (interactive)
   (find-file (concat (getenv "APPDATA") "/Code/User/settings.json")))
 
-(defun find-file-vscode-keybindings ()
+(defun _find-file-vscode-keybindings ()
   "Opens vscode keybindings.json."
   (interactive)
   (find-file (concat (getenv "APPDATA") "/Code/User/keybindings.json")))
 
-(defun find-file-pwsh-profile ()
+(defun _find-file-pwsh-profile ()
   "Opens powershell.ps1"
   (interactive)
   (find-file
@@ -85,7 +86,7 @@
     (getenv "OneDrive")
     "/Documents/PowerShell/Microsoft.PowerShell_profile.ps1")))
 
-(defun copilot-turn-on-unless-buffer-read-only ()
+(defun _copilot-turn-on-unless-buffer-read-only ()
   "Turn on `copilot-mode' if the buffer is writable."
   (unless (or buffer-read-only (not (buffer-file-name (current-buffer))))
     (copilot-mode 1)))
@@ -94,37 +95,58 @@
   :ensure t
   :config
   (general-define-key
-   "C-c e"      'find-file-emacs-init
+   "C-c e"      '_find-file-emacs-init
    "M-<up>"     'windmove-up
    "M-<down>"   'windmove-down
    "M-<left>"   'windmove-left
    "M-<right>"  'windmove-right
-   "C-<down>"   'move-line-down
-   "C-<up>"     'move-line-up
-   "M-i"        'indent-to-next-tab-stop))
+   "C-<down>"   '_move-line-down
+   "C-<up>"     '_move-line-up
+   "M-i"        '_indent-to-next-tab-stop))
 
 (use-package savehist
+  :ensure nil
   :init
   (savehist-mode))
+
+(use-package recentf
+  :ensure nil 
+  :hook (after-init . recentf-mode)
+  :custom
+  (recentf-max-saved-items 100) 
+  (recentf-auto-cleanup 'never) 
+  (recentf-save-file (expand-file-name "recentf" user-emacs-directory)) 
+  :config
+  (setq recentf-exclude '("\\.git.*" "node_modules" "/tmp/"))
+  (run-at-time nil (* 5 60) 'recentf-save-list)
+  :bind
+  (("C-x C-r" . consult-recent-file)))
 
 (use-package format-all
   :ensure t
   :commands format-all-mode
   :hook
   (prog-mode . format-all-mode)
+  (emacs-lisp-mode . (lambda () (add-hook 'before-save-hook 'format-all-buffer nil 'local)))
   :config
   (setq-default format-all-formatters
                 '(("Python" ruff "--format"))))
 
+
 (use-package ispell
   :init
-  (setq ispell-program-name "aspell" ispell-dictionary "british"))
+  (setq
+   ispell-program-name "C:/msys64/mingw64/bin/hunspell"
+   ispell-dictionary "en_US"
+   ispell-personal-dictionary (expand-file-name "~/.hunspell_en_US")
+   ispell-extra-args '("-d" "en_US" "-a")))
 
 (use-package which-key
   :ensure t
   :init
   (which-key-mode)
   :config
+  (setq which-key-min-display-lines 9)
   (setq which-key-idle-delay 0.1))
 
 (use-package rainbow-delimiters
@@ -159,12 +181,14 @@
 
 (use-package consult
   :ensure t
+  :after vertico
   :bind
   ("C-c b" . consult-buffer)
-  ("C-c s" . consult-isearch)
+  ("C-c s" . consult-isearch-forward)
   ("C-c r" . consult-ripgrep)
   ("C-c l" . consult-line)
   ("C-c f" . consult-find)
+  ("C-c F" . consult-flyspell)
   :config
   (setq consult-preview-buffer-height 15))
 
@@ -190,14 +214,9 @@
    corfu-auto-prefix 1)
   (add-to-list
    'corfu-margin-formatters #'nerd-icons-corfu-formatter))
-
+ 
 (use-package nerd-icons-corfu
   :ensure t)
-
-(use-package flycheck
-  :ensure t
-  :init
-  (global-flycheck-mode))
 
 (use-package eglot
   :ensure t
@@ -205,7 +224,10 @@
   (python-mode . eglot-ensure)
   (rust-mode . eglot-ensure)
   :config
-  (setq eglot-connect-timeout 5000)
+  (setq eglot-connect-timeout nil)
+  (setq eglot-autoshutdown t)
+  (setq eglot-sync-connect nil)
+  (setq eglot-ignored-server-capabilities '(:hoverProvider :documentLinkProvider :inlayHintProvider :documentOnTypeFormattingProvider))
   (add-to-list 'eglot-server-programs '(rust-mode .
                                                   ("rust-analyzer" :initializationOptions (:check (:command "clippy")))))
   (add-to-list 'eglot-server-programs '(python-mode .
@@ -221,30 +243,46 @@
   ((prog-mode powershell-mode yaml-mode) .
    (lambda () (tree-sitter-mode) (tree-sitter-hl-mode))))
 
-(use-package tree-sitter-langs
-  :ensure t
-  :after tree-sitter)
+;;(use-package tree-sitter-langs
+;;  :ensure t
+;;  :after tree-sitter)
 
 (use-package flyspell
   :ensure t
   :bind
-  (:map flyspell-mode-map
-        ("C-." . flyspell-correct-word-before-point))
   :hook
-  ((text-mode . flyspell-mode)
-   (prog-mode . flyspell-prog-mode))
+  ((text-mode . flyspell-mode) 
+   (prog-mode . flyspell-prog-mode)) 
   :config
-  (dolist (additional-faces
-           '(font-lock-string-face font-lock-comment-face font-lock-doc-face
-                                   tree-sitter-hl-face:comment tree-sitter-hl-face:string tree-sitter-hl-face:doc))
-    (add-to-list 'flyspell-prog-text-faces additional-faces)))
+  (dolist (face '(font-lock-string-face
+                  font-lock-comment-face
+                  font-lock-doc-face
+                  tree-sitter-hl-face:comment
+                  tree-sitter-hl-face:string
+                  tree-sitter-hl-face:doc))
+    (add-to-list 'flyspell-prog-text-faces face)))
+
+(use-package flyspell-correct
+  :ensure t
+  :after flyspell
+  :bind
+  (:map flyspell-mode-map
+        ("C-," . flyspell-correct-wrapper))
+  :config
+  (setq flyspell-correct-interface #'flyspell-correct-completing-read))
 
 (use-package yasnippet
   :ensure t
   :hook
-  (prog-mode . yas-minor-mode)
+  ((prog-mode . yas-minor-mode)
+   (org-mode . yas-minor-mode))
   :config
-  (yas-reload-all))
+  (yas-reload-all)
+  :bind
+  (:map yas-minor-mode-map
+        ("TAB" . yas-expand) 
+        ("C-c y" . yas-insert-snippet) 
+        ("C-c n" . yas-new-snippet)))
 
 (use-package rust-mode
   :ensure t
@@ -322,21 +360,84 @@
   :mode
   ("\\.yml\\'\\|\\.yaml\\'\\|/\\.condarc\\'" . yaml-mode))
 
-(use-package cape
+(use-package org
+  :ensure t
+  ;;  :hook
+  ;;  (org-mode . yas-minor-mode)
+  :config
+  (setq org-image-actual-width 400)
+  (setq org-highlight-latex-and-related '(latex))
+  (setq org-latex-default-packages-alist nil)
+  (setq org-cite-export-processors
+        '((latex biblatex)
+          (html  citeproc)
+          (t     basic)))
+  (setq org-latex-listings 'minted)
+  (add-to-list 'org-latex-packages-alist '("" "minted"))
+  (setq org-confirm-babel-evaluate nil)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (python . t)
+     (jupyter . t))))
+
+(use-package citar
+  :ensure t
+  :custom
+  (citar-bibliography 
+    (append 
+      (list (concat (getenv "OneDrive") "/notes/papers/bibliography.bib"))
+      (directory-files default-directory t "\\.bib$")))
+ :hook
+  ((org-mode . (lambda ()
+                 (evil-define-key 'normal org-mode-map
+                   (kbd "C-c c") 'citar-insert-citation
+                   (kbd "C-c b") 'citar-insert-bibliography
+                   (kbd "C-c r") 'citar-refresh
+                   (kbd "C-c s") 'citar-open)))
+   (TeX-mode . (lambda ()
+                 (evil-define-key 'normal TeX-mode-map
+                   (kbd "C-c c") 'citar-insert-citation
+                   (kbd "C-c b") 'citar-insert-bibliography
+                   (kbd "C-c r") 'citar-refresh
+                   (kbd "C-c s") 'citar-open)))))
+
+(use-package org-download
+  :ensure t
+  :after org
+  :config
+  (setq org-download-image-dir "./images")
   :bind
-  (("M-p p" . completion-at-point)
-   ("M-p d" . cape-dabbrev)
-   ("M-p k" . cape-keyword)
-   ("M-p \\". cape-tex)
-   ("M-p l" . cape-line)
-   ("M-p f" . cape-file)
-   ("M-p e" . cape-elisp-block)
-   ("M-p h" . cape-history)
-   ("M-p w" . cape-dict))
-  :init
-  (dolist (func '(cape-dabbrev cape-file cape-elisp-block
-                               cape-history cape-keyword cape-dict))
-    (add-to-list 'completion-at-point-functions func)))
+  ("C-M-y" . org-download-clipboard))
+
+(use-package auctex
+  :ensure t
+  :defer t
+  :hook (LaTeX-mode . turn-on-reftex))
+
+(use-package cdlatex
+  :ensure t
+  :hook
+  (org-mode . turn-on-org-cdlatex)
+  (LaTeX-mode . turn-on-cdlatex))
+
+
+;;(use-package cape
+;; :ensure t
+;; :bind
+;; (("M-p p" . completion-at-point)
+;;  ("M-p d" . cape-dabbrev)
+;;  ("M-p k" . cape-keyword)
+;;  ("M-p \\". cape-tex)
+;;  ("M-p l" . cape-line)
+;;  ("M-p f" . cape-file)
+;;  ("M-p e" . cape-elisp-block)
+;;  ("M-p h" . cape-history)
+;;  ("M-p w" . cape-dict)
+;; :init
+;; (dolist (func '(cape-dabbrev cape-file cape-elisp-block
+;;                              cape-history cape-keyword cape-dict))
+;;   (add-to-list 'completion-at-point-functions func))))
 
 (add-to-list 'load-path "~/.emacs.d/copilot")
 (use-package copilot
@@ -368,16 +469,18 @@
     (kbd "<leader>fs") #'save-buffer
     (kbd "<leader>ff") #'find-file
     (kbd "<leader>fl") #'load-file
-    (kbd "<leader>fi") #'find-file-emacs-init
+    (kbd "<leader>fi") #'_find-file-emacs-init
     (kbd "<leader>fp") #'package-install
+    (kbd "<leader>fP") #'list-packages
     (kbd "<leader>fr") #'revert-buffer)
 
   (evil-define-key 'normal 'global
     (kbd "<leader>s")  #'isearch-forward
     (kbd "<leader>S")  #'isearch-backward
-    (kbd "<leader>cl") #'consult-line
+    (kbd "<leader>l") #'consult-line
     (kbd "<leader>cb") #'consult-buffer
-    (kbd "<leader>cr") #'consult-ripgrep)
+    (kbd "<leader>cr") #'consult-ripgrep
+    (kbd "<f13> l") #'consult-line)
 
   (evil-define-key 'normal 'global
     (kbd "TAB") #'indent-for-tab-command
@@ -389,11 +492,20 @@
     (kbd "C-a") #'beginning-of-line
     (kbd "C-e") #'end-of-line))
 
-(use-package <>evil</>-collection
+
+(use-package evil-collection
   :after evil
   :ensure t
   :config
-  (evil-collection-init))
+  (evil-collection-init)
+  (with-eval-after-load 'package
+    (evil-define-key 'normal package-menu-mode-map
+      (kbd "f n") 'package-menu-filter-by-name
+      (kbd "f c") 'package-menu-clear-filter
+      (kbd "f m") 'package-menu-filter-marked
+      (kbd "f u") 'package-menu-filter-upgradable
+      (kbd "f i") 'package-menu-filter-description
+      (kbd "f s") 'package-menu-filter-status)))
 
 (use-package evil-surround
   :ensure t
@@ -416,7 +528,6 @@
   :ensure t
   :config
   (load-theme 'doom-gruvbox t)
-  (doom-themes-org-config)
   (setq doom-gruvbox-dark-variant "hard"))
 
 (use-package emacs
@@ -425,6 +536,7 @@
   (column-number-mode 1)
   (global-auto-revert-mode 1)
   (electric-pair-mode 1)
+  (global-set-key [remap dabbrev-expand] 'hippie-expand)
   (setq
    inhibit-splash-screen t
    backup-inhibited t
@@ -459,3 +571,4 @@
 (if (daemonp)
     (add-hook 'after-make-frame-functions #'initialize-frame)
   (initialize-frame (selected-frame)))
+(put 'downcase-region 'disabled nil)
